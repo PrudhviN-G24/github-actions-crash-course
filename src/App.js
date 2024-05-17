93f4267f09da5abd8bafb318d1fdfca552e80109
PrudhviN-G24 <122725748+PrudhviN-G24@users.noreply.github.com>
2024-05-17 20:46:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn DevOps
+          Learn DevOps - Prudhvi
         </a>
       </header>
     </div>
